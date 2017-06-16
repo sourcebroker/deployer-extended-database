000fbf7bbf9221cb3bfe7f2ddb1039dc62256a86
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2017-06-16 15:27:46 +0200

    Add missing "current_server" var after decoulpling from deployer-extended.

--- deployer/db/config/set.php
+++ deployer/db/config/set.php
@@ -111,3 +111,18 @@ set('bin/deployer', function () {
     }
     return $deployerBin;
 });
+
+set('current_server', function () {
+    try {
+        $currentServer = Deployer::get()->environments[get('instance')];
+    } catch (\RuntimeException $e) {
+        $servers = '';
+        $i = 1;
+        foreach (Deployer::get()->environments as $key => $server) {
+            $servers .= "\n" . $i++ . '. ' . $key;
+        }
+        throw new \RuntimeException("Name of instance \"" . get('instance') . "\" is not on the server list:" . $servers
+            . "\nPlease check case sensitive. [Error code: 1458412947]");
+    }
+    return $currentServer;
+});
