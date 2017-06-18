e89091a77177824febeb15f2b3871e2c8a2f7356
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2017-06-18 11:26:08 +0200

    [FEATURE] Add env standard driver.

---
+++ src/Driver/EnvDriver.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace SourceBroker\DeployerExtendedDatabase\Driver;
+
+use Dotenv\Dotenv;
+
+/**
+ * Configuration reader for database data stored in .env file.
+ *
+ * Class EnvDriver
+ * @package SourceBroker\DeployerExtendedDatabase\Driver
+ */
+class EnvDriver
+{
+    /**
+     * @param string $prefix
+     * @param null $absolutePath
+     * @return array
+     * @throws \Exception
+     * @internal param null $params
+     */
+    public function getDatabaseConfig($prefix = '', $absolutePath = null)
+    {
+        $absolutePath = null === $absolutePath ? getcwd() : $absolutePath;
+        $absolutePath = rtrim($absolutePath, DIRECTORY_SEPARATOR);
+        if (file_exists($absolutePath . '/.env')) {
+            (new Dotenv($absolutePath))->load();
+            foreach (['DATABASE_HOST', 'DATABASE_NAME', 'DATABASE_USER', 'DATABASE_PASSWORD'] as $requiredEnv) {
+                if (false === getenv($prefix . $requiredEnv)) {
+                    throw new \Exception('Missing ' . $prefix . $requiredEnv . ' in .env file.');
+                }
+            }
+            return [
+                'host' => getenv($prefix . 'DATABASE_HOST'),
+                'port' => getenv($prefix . 'DATABASE_PORT') ? getenv($prefix . 'DATABASE_PORT') : 3306,
+                'dbname' => getenv($prefix . 'DATABASE_NAME'),
+                'user' => getenv($prefix . 'DATABASE_USER'),
+                'password' => getenv($prefix . 'DATABASE_PASSWORD')
+            ];
+        } else {
+            throw new \Exception('Missing file "' . getcwd() . '/.env');
+        }
+    }
+}
