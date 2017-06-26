58f68aee9f247751357bf63db88eda8ef82629f5
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2017-06-26 14:25:51 +0200

    [BUGFIX] Refactor db:copy task - fix for wrong path on import command

--- deployer/db/task/db:copy.php
+++ deployer/db/task/db:copy.php
@@ -46,6 +46,6 @@ task('db:copy', function () {
     } else {
         runLocally("{{local/bin/deployer}} db:upload $targetInstanceName --dumpcode=$dumpCode", 0);
         run("cd " . $targetInstanceEnv->get('deploy_path') . "/current && " . $targetInstanceEnv->get('bin/php') .
-            $targetInstanceEnv->get('bin/deployer') . " -q db:import --dumpcode=" . $dumpCode);
+           ' ' .  $targetInstanceEnv->get('bin/deployer') . " -q db:import --dumpcode=" . $dumpCode);
     }
 })->desc('Synchronize database between instances.');
