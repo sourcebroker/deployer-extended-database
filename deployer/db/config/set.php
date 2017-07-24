a7187cf3c267534a0a38103a57248df822676850
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2017-07-24 22:31:22 +0200

    [TASK] Make "bin/deployer" use of vendor/bin/dep from deployer-dist

--- deployer/db/config/set.php
+++ deployer/db/config/set.php
@@ -113,85 +113,16 @@ set('db_storage_path', function () {
 });
 
 set('bin/deployer', function () {
-    $activePath = get('deploy_path') . '/' . (test('[ -L {{deploy_path}}/release ]')
-            ? 'release' : 'current');
-    //check if there is composer based deployer
+    $activePath = get('deploy_path') . '/' . (test('[ -L {{deploy_path}}/release ]') ? 'release' : 'current');
+    // We need check if there is composer based deployer
     if (test('[ -e ' . escapeshellarg($activePath . '/vendor/bin/dep') . ' ]')) {
         $deployerBin = $activePath . '/vendor/bin/dep';
     } else {
-        $deployerMinimumSizeInBytesForDownloadCheck = 100000;
-        // Figure out what version is expected to be used. "db_deployer_version" can be either integer "4"
-        // or full verison "4.3.0". There is no support for "4.3".
-        // TODO: Find the most recent versions automaticaly
-        $deployerVersionRequestedByUser = trim(get('db_deployer_version'));
-        if (strpos($deployerVersionRequestedByUser, '.') === false) {
-            switch (($deployerVersionRequestedByUser)) {
-                case 5:
-                    $deployerVersionToUse = '5.0.3';
-                    break;
-                case 4:
-                    $deployerVersionToUse = '4.3.1';
-                    break;
-                case 3:
-                    $deployerVersionToUse = '3.3.0';
-                    break;
-                case 2:
-                    $deployerVersionToUse = '2.0.5';
-                    break;
-                default:
-                    throw new \RuntimeException('You requested deployer version "' . $deployerVersionRequestedByUser . '" 
-                but we are not able to determine what exact version is supposed to be chosen. 
-                Please set "db_deployer_version" to full semantic versioning like "' . $deployerVersionRequestedByUser . '.0.1"',
-                        1500717666998);
-            }
-        } else {
-            if (count(explode('.', $deployerVersionRequestedByUser)) === 3) {
-                $deployerVersionToUse = $deployerVersionRequestedByUser;
-            } else {
-                throw new \RuntimeException('Deployer version must be set to just "major" like "4" which means give me most '
-                    . 'fresh version for deployer 4 or "major.minor.path" like "4.3.0". The "' . $deployerVersionRequestedByUser
-                    . '" is not supported.', 1500717685169);
-            }
-        }
-        $deployerFilename = 'deployer-' . $deployerVersionToUse . '.phar';
-        $deployerFilenameFullPath = get('deploy_path') . '/shared/' . $deployerFilename;
-        if (test('[ -f ' . $deployerFilenameFullPath . ' ]')) {
-            $downloadedFileSizeInBytes = trim(run('wc -c  < {{deploy_path}}/shared/' . $deployerFilename)->toString());
-            if ($downloadedFileSizeInBytes < $deployerMinimumSizeInBytesForDownloadCheck) {
-                writeln(parse('Removing {{deploy_path}}/shared/' . $deployerFilename . ' because the file size when last '
-                    . 'downloaded was ' . $downloadedFileSizeInBytes . ' bytes so downloads was probably not sucessful.'));
-                run('rm -f {{deploy_path}}/shared/' . $deployerFilename);
-            }
-        }
-        //If we do not have yet this version fo deployer in shared then download it
-        if (!test('[ -f ' . $deployerFilenameFullPath . ' ]')) {
-            $deployerDownloadLink = 'https://deployer.org/releases/v' . $deployerVersionToUse . '/deployer.phar';
-            run('cd {{deploy_path}}/shared && curl -o ' . $deployerFilename . ' -L ' . $deployerDownloadLink . ' && chmod 775 ' . $deployerFilename);
-            //Simplified checker if deployer has been downloaded or not. Just check size of file which should be at least 200kB.
-            $downloadedFileSizeInBytes = trim(run('wc -c  < ' . $deployerFilenameFullPath)->toString());
-            if ($downloadedFileSizeInBytes > $deployerMinimumSizeInBytesForDownloadCheck) {
-                run("cd {{deploy_path}}/shared && chmod 775 " . $deployerFilename);
-            } else {
-                throw new \RuntimeException(parse('Downloaded deployer has size ' . $downloadedFileSizeInBytes . ' bytes. It seems'
-                    . ' like the download was unsucessfull. The file downloaded was: "' . $deployerDownloadLink . '".'
-                    . 'Please check if this link return file. The downloaded content was stored in ' . $deployerFilenameFullPath),
-                    1500717708109);
-            }
-        }
-        //Rebuild symlink of $deployerFilename to $active_path/deployer.phar
-        run('rm -f ' . $activePath . '/deployer.phar && cd ' . $activePath . ' && {{bin/symlink}} ' . $deployerFilenameFullPath . ' ' . $activePath . '/deployer.phar');
-        if (test('[ -f ' . $activePath . '/deployer.phar ]')) {
-            $deployerBin = $activePath . '/deployer.phar';
-        } else {
-            throw new \RuntimeException(parse('Can not create symlink from ' . $deployerFilenameFullPath . ' to ' . $activePath . '/deployer.phar'));
-        }
+        throw new \RuntimeException('There must be ' . $activePath . '/vendor/bin/dep phar file, but it could not be found.');
     }
     return $deployerBin;
 });
 
-// Local call of deployer can be not standard. For example someone could have "dep3" and "dep4" symlinks and call
-// "dep3 deploy live". He could expect then that if we will use deployer call inside task we will use then "dep3" and not "dep"
-// so we store actual way of calling deployer into "local/bin/deployer" var to use it whenever we call local deployer again in tasks.
 set('local/bin/deployer', function () {
     if ($_SERVER['_'] == $_SERVER['PHP_SELF']) {
         return $_SERVER['_'];
