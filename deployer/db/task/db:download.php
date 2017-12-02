310fc4dd05d0784fe4224e6ff9c2aebe150139ce
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2017-12-02 17:34:54 +0100

    [TASK] Cleanup for db:upload, db:download tasks

--- deployer/db/task/db:download.php
+++ deployer/db/task/db:download.php
@@ -4,23 +4,24 @@ namespace Deployer;
 
 use SourceBroker\DeployerExtendedDatabase\Utility\ConsoleUtility;
 use SourceBroker\DeployerExtendedDatabase\Utility\FileUtility;
+use SourceBroker\DeployerExtendedDatabase\Utility\RsyncUtility;
 
 /*
  * @see https://github.com/sourcebroker/deployer-extended-database#db-download
  */
 task('db:download', function () {
-    $fileUtility = new FileUtility();
     if (null === input()->getArgument('stage')) {
         throw new \RuntimeException('The source instance is required for db:download command.', 1488143750580);
     }
     $dumpCode = (new ConsoleUtility())->optionRequired('dumpcode', input());
-    $currentInstanceDatabaseStoragePath = $fileUtility->normalizeFolder(get('db_current_server')->get('db_storage_path_current'));
-    $rsyncUtility = new \SourceBroker\DeployerExtendedDatabase\Utility\RsyncUtility();
+    $rsyncUtility = new RsyncUtility();
+    $fileUtility = new FileUtility();
     runLocally(sprintf(
-        'rsync -rz --remove-source-files %s --include=*dumpcode:%s*.sql --exclude=* %s %s',
-        $rsyncUtility->getSshOptions(Task\Context::get()) ? '-e ' . escapeshellarg($rsyncUtility->getSshOptions(Task\Context::get())) : '',
-        $dumpCode,
+        'rsync -rz --remove-source-files %s --include=%s --exclude=* %s %s',
+        $rsyncUtility->getSshOptions(Task\Context::get()) ? '-e '
+            . escapeshellarg($rsyncUtility->getSshOptions(Task\Context::get())) : '',
+        escapeshellarg('*dumpcode:' . $dumpCode . '*'),
         escapeshellarg($rsyncUtility->getHostWithDbStoragePath(Task\Context::get())),
-        escapeshellarg($currentInstanceDatabaseStoragePath)
+        escapeshellarg($fileUtility->normalizeFolder(get('db_current_server')->get('db_storage_path_current')))
     ), 0);
 })->desc('Download the database dumps with given dumpcode from target to current database dumps storage.');
