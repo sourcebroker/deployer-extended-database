f5fc10eb687afbb6f91028ada3d0a35518371166
Sebastian Blank <blank@data-factory.net>
2020-09-10 17:30:31 +0200

    Compatibility with Symfony 3

--- src/Driver/EnvDriver.php
+++ src/Driver/EnvDriver.php
@@ -23,18 +23,28 @@ class EnvDriver
         $absolutePath = null === $absolutePath ? getcwd() : $absolutePath;
         $absolutePath = rtrim($absolutePath, DIRECTORY_SEPARATOR);
         $envFilePath = $absolutePath . '/.env';
-        (new Dotenv())->loadEnv($envFilePath);
-        foreach (['DATABASE_HOST', 'DATABASE_NAME', 'DATABASE_USER', 'DATABASE_PASSWORD'] as $requiredEnv) {
-            if (false === getenv($prefix . $requiredEnv)) {
-                throw new \Exception('Missing ' . $prefix . $requiredEnv . ' in .env file.');
+        if (file_exists($envFilePath)) {
+            $dotEnv = new Dotenv();
+            if (method_exists($dotEnv, 'loadEnv')) {
+                // Symfony => 4.2 style
+                $dotEnv->loadEnv($envFilePath);
+            } else {
+                $dotEnv->load($envFilePath);
             }
+            foreach (['DATABASE_HOST', 'DATABASE_NAME', 'DATABASE_USER', 'DATABASE_PASSWORD'] as $requiredEnv) {
+                if (false === getenv($prefix . $requiredEnv)) {
+                    throw new \Exception('Missing ' . $prefix . $requiredEnv . ' in .env file.');
+                }
+            }
+            return [
+                'host' => getenv($prefix . 'DATABASE_HOST'),
+                'port' => getenv($prefix . 'DATABASE_PORT') ?: 3306,
+                'dbname' => getenv($prefix . 'DATABASE_NAME'),
+                'user' => getenv($prefix . 'DATABASE_USER'),
+                'password' => getenv($prefix . 'DATABASE_PASSWORD')
+            ];
         }
-        return [
-            'host' => getenv($prefix . 'DATABASE_HOST'),
-            'port' => getenv($prefix . 'DATABASE_PORT') ?: 3306,
-            'dbname' => getenv($prefix . 'DATABASE_NAME'),
-            'user' => getenv($prefix . 'DATABASE_USER'),
-            'password' => getenv($prefix . 'DATABASE_PASSWORD')
-        ];
+
+        throw new \Exception('Missing file "' . $envFilePath);
     }
 }
