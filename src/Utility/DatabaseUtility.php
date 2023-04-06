974c4c8988dae5d79473b87641cd0b2697823f60
Maik Schneider <schneider.maik@me.com>
2023-04-06 15:11:27 +0200

    [FEATURE] Read SSL and mysqli driver options from Env, use new settings in DatabaseUtility

--- src/Utility/DatabaseUtility.php
+++ src/Utility/DatabaseUtility.php
@@ -6,9 +6,22 @@ class DatabaseUtility
 {
     public function getTables(array $dbConf): array
     {
-        $link = mysqli_connect($dbConf['host'], $dbConf['user'], $dbConf['password'], $dbConf['dbname'],
+        $mysqli = new \mysqli($dbConf['host'], $dbConf['user'], $dbConf['password'], $dbConf['dbname'],
             $dbConf['port']);
-        $result = $link->query('SHOW TABLES');
+
+        $mysqli->ssl_set(
+            $dbConf['ssl_key'] ?? null,
+            $dbConf['ssl_cert'] ?? null,
+            $dbConf['ssl_ca'] ?? null,
+            $dbConf['ssl_capath'] ?? null,
+            $dbConf['ssl_cipher'] ?? null
+        );
+
+        foreach ($dbConf['options'] ?? [] as $optionName => $value) {
+            $mysqli->options($optionName, $value);
+        }
+
+        $result = $mysqli->query('SHOW TABLES');
         $allTables = [];
         while ($row = $result->fetch_row()) {
             $allTables[] = array_shift($row);
